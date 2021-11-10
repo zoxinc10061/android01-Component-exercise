e1a071e43c6b6ad85159ae9c80e71058170d4050
zxc87120521 <asd87120521@gmail.com>
2021-11-10 09:58:23 +0800

    first commit

---
+++ app/src/main/java/com/example/a01/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.a01;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void login_in(View view){
+        Button btn = findViewById(R.id.log_in);
+        btn.setText("logout");
+    }
+}
